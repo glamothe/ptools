396deadc411f701ff9c536b2ecd7069e96abb495
saladin <>
2006-12-21 16:49:19 +0000

    added some useful functions for Coord3D. Not pythonized yet...

--- coord3d.h
+++ coord3d.h
@@ -2,12 +2,88 @@
 #define COORD3D_H
 
 
+#include <math.h>
+
 namespace PTools{
 
 struct Coord3D
 {
 double x,y,z;
+
+
+    
+//     inline double Norm(const coord3D & A);
+//     inline double Norm2(const coord3D& A);
+//     inline coord3D operator* (const coord3D& A, double scal);
+//     inline coord3D operator* (double scal, const coord3D& A);
+//     inline coord3D operator/ (const coord3D& A, double scal);
+    inline Coord3D & operator= (const Coord3D &);
+
 };
+
+
+inline Coord3D & Coord3D::operator= (const Coord3D & C)
+{
+    x = C.x;
+    y = C.y;
+    z = C.z;
+    return *this;
+}
+
+
+inline Coord3D operator+ (const Coord3D& A,const Coord3D& B)
+{
+    Coord3D P;
+    P.x = A.x + B.x ;
+    P.y = A.y +  B.y ;
+    P.z = A.z + B.z ;
+    return P;
+}
+
+inline Coord3D operator- (const Coord3D& A,const Coord3D& B)
+{
+    Coord3D P;
+    P.x = A.x - B.x ;
+    P.y = A.y -  B.y ;
+    P.z = A.z - B.z ;
+    return P;
+}
+
+inline double Norm(const Coord3D & A)
+{
+    return sqrt (A.x*A.x + A.y*A.y + A.z*A.z) ;
+}
+
+inline double Norm2(const Coord3D & A)
+{
+    return  (A.x*A.x + A.y*A.y + A.z*A.z);
+}
+
+inline Coord3D operator* (const Coord3D& A, double scal)
+{
+    Coord3D P;
+    P.x = A.x * scal ;
+    P.y = A.y * scal ;
+    P.z = A.z * scal ;
+    return P;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 };
 
