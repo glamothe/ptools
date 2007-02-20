7dc596f03eb6431b7d5b3d954ca2012a5ae844c3
poulain <>
2007-02-20 15:38:34 +0000

    Je m'appelle Pom Le Poulain ...

---
+++ ptools.h
@@ -0,0 +1,15 @@
+//
+// C++ Interface: generic interface for ptools 
+//
+
+#ifndef PTOOLS_H
+#define PTOOLS_H
+
+#include "coord3d.h"
+#include "atom.h"
+#include "rigidbody.h"
+#include "geometry.h"
+#include "atomselection.h"
+#include "pdbio.h"
+
+#endif // PTOOLS_H
