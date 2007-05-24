84ca25a5d928ccf983d41aebf695af7a341319bd
saladin <>
2007-05-24 12:06:55 +0000

    code cleaning + minor output modifications during minimizations

--- minimizers/lbfgs_interface.cpp
+++ minimizers/lbfgs_interface.cpp
@@ -33,52 +33,6 @@ void Lbfgs::minimize(int maxiter)
 {
 
 
-//     double f;
-//     int j;
-// 
-//     int icall;
-//     //double x[N];
-//     x.resize(n);
-//     //double g[N];
-//     g.resize(n);
-// 
-//     lbfgs_t* opt;
-// 
-//     opt = lbfgs_create(n, 10, 1.0E-3);  //n:proble size, 5: number of memory corrections, 1e-5: eps.
-//     opt->iprint[0] = 0;  //be a little bit verbose
-//     opt->iprint[1] = 0;  // TODO: add comment
-//     opt->diagco = 0;    // ? -> TODO: add comment
-// 
-// 
-//    for (j = 0; j < n; j++) {
-//     x[j]=0.0;
-//     }
-// 
-//     icall = 0;
-//     while (1) {
-//               //f and g evaluation:
-// 
-//                 f = objToMinimize.Function(x);
-//                 //objToMinimize.NumDerivatives(x,g);
-//                 //std::cout << " numerical grad: "; for(uint i=0; i<6;i++) std::cout << g[i] << "  "; std::cout <<"\n";
-//                 objToMinimize.Derivatives(x,g);
-// //                 std::cout << "analytical grad: "; for(uint i=0; i<6;i++) std::cout << g[i] << "  "; std::cout <<"\n";
-// 
-//         
-//         if (lbfgs_run(opt, &x[0], &f, &g[0]) <= 0)
-//             break;
-//         ++icall;
-//         // We allow at most 2000 evaluations of F and G 
-//         if (icall > 2000)
-//             break;
-//     }
-// 
-//     lbfgs_destroy(opt);
-
-
-
-
-
     int n = objToMinimize.ProblemSize();
     std::cout  << "Problem size: " << n << std::endl;
 
@@ -108,7 +62,7 @@ void Lbfgs::minimize(int maxiter)
     lbfgsb_t* opt = lbfgsb_create(n, m, &l[0], &u[0], &nbd[0]);
     assert(opt);
 
-    opt->iprint=1;
+    opt->iprint=-1;
 
     double f = 0.0;
 
@@ -139,7 +93,7 @@ void Lbfgs::minimize(int maxiter)
     opt->task[59]='\0'; //add a null terminating character to task[]
 
 
-    std::cout << opt->task <<"\n";
+    std::cout << opt->task  << " |  " << opt->niter << " iterations\n";
     lbfgsb_destroy(opt);
 
 
