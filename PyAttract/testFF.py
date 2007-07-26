4b9efee2a402ef5c37d070456996485ca38cc897
saladin <>
2007-07-26 18:58:58 +0000

    testFF (test forcefield) now working again with new python interface

--- PyAttract/testFF.py
+++ PyAttract/testFF.py
@@ -4,10 +4,10 @@ from ptools import *
 import sys
 
 
-rig = Rigidbody("1FIN_c_r.red")
-rig2 = Rigidbody("1FIN_c_l.red")
+#rig = Rigidbody("1FIN_c_r.red")
+#rig2 = Rigidbody("1FIN_c_l.red")
 
-FF=AttractForceField(rig2,rig,10)
+#FF=AttractForceField(rig2,rig,10)
 
 #FF=TestForceField()
 
@@ -21,9 +21,11 @@ class PyForceField(ForceField):
     def __init__(self):
         ForceField.__init__(self)
     def Function(self, vec):
+	print "============="
         print "len(vec): ", len(vec)
         sol=(vec[0]-3)**2
         print "fonction: ", sol
+	print "vector: ", list(vec)
         return sol
     def Derivatives(self, vec1, grad):
         #grad[0]=2*vec1[0]-6
@@ -35,7 +37,7 @@ class PyForceField(ForceField):
     def ProblemSize(self):
         return 1
 
-#FF=PyForceField()
+FF=PyForceField()
 
 
 
@@ -46,8 +48,8 @@ class PyForceField(ForceField):
 minim=Lbfgs(FF)
 minim.minimize(500)
 
-X=std_vector_double()
-G=std_vector_double()
+X=Vdouble()
+G=Vdouble()
 
 def initvec(vec):
 	for i in range(6):
