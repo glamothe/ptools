0bf6d6649ccc71f0cc417c7885f5ca11518e3fe5
saladin <>
2007-07-26 14:36:30 +0000

    maybe a solution for the "ugly aliases" found for vector <double> on some machines (std_vector_less_double...)

--- py_details.h
+++ py_details.h
@@ -1,6 +1,18 @@
 #include "pairlist.h"
 #include "ptools.h"
 
+#include <vector>
+
+namespace pyplusplus{ namespace aliases{
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    typedef std::vector< double > Vdouble;
+    typedef std::vector< uint > Vuint ;
+    typedef std::vector< int > Vint ;
+    } } //pyplusplus::aliases
+
+
+
+
 namespace  py_details{
     inline void instantiate(){
         using namespace PTools;
