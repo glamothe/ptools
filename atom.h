e610921758842b34e9179eb7de417320f0f2da82
saladin <>
2007-08-07 07:15:40 +0000

    doxygen didn't work with the old comments

--- atom.h
+++ atom.h
@@ -25,35 +25,59 @@ private:
     std::string mExtra; ///< extra data
 
 public:
-    Atomproperty(){ mAtomType="X"; ///< default constructor
-                                  mAtomElement="X";
-                                  mResidType="XXX";
-                                  mChainId="X";
-                                  mResidId=1;
-                                  mAtomId=1;
-                                  mAtomCharge=0.0;
+    /// default constructor
+    Atomproperty() 
+    {
+        mAtomType="X"; 
+        mAtomElement="X";
+        mResidType="XXX";
+        mChainId="X";
+        mResidId=1;
+        mAtomId=1;
+        mAtomCharge=0.0;
     };
 
-    std::string GetType() const {return mAtomType;}; ///< return atom type
-    void SetType(std::string newtype) { mAtomType = newtype;}; ///< define atom type
+    /// return atom type const
+    std::string GetType()  {return mAtomType;}; 
 
-    std::string GetResidType() const {return mResidType;}; ///< return residue type
-    void SetResidType(std::string residtype){mResidType=residtype;}; ///< define residue type
+    /// define atom type
+    void SetType(std::string newtype) { mAtomType = newtype;}; 
 
-    inline double GetAtomCharge() const {return mAtomCharge;}; ///< return atom charge
-    inline void SetAtomCharge(double ch) {mAtomCharge=ch;}; ///< define atom charge
+    /// return residue type (LEU, ARG, ...)
+    std::string GetResidType() const {return mResidType;}; 
 
-    inline std::string GetChainId() const {return mChainId;}; ///< return chain ID
-    inline void SetChainId(std::string chainid) {mChainId=chainid;}; ///< define chain ID
+    /// define residue type
+    void SetResidType(std::string residtype){mResidType=residtype;}; 
 
-    inline uint GetResidId() const {return mResidId;}; ///< return residue ID
-    inline void SetResidId(uint id) {mResidId = id;}; ///< define residue ID
+    /// return atom charge
+    inline double GetAtomCharge() const {return mAtomCharge;}; 
 
-    inline uint GetAtomId() const {return mAtomId;}; ///< return atom ID
-    inline void SetAtomId(uint atomnumber) {mAtomId=atomnumber;}; ///< define atom ID
+    /// define atom charge
+    inline void SetAtomCharge(double ch) {mAtomCharge=ch;}; 
 
-    inline void SetExtra(std::string extra){mExtra=extra;}; ///< set the extra data field
-    inline std::string GetExtra() const {return mExtra;}; ///< get the extra data field
+    /// return chain ID
+    inline std::string GetChainId() const {return mChainId;}; 
+
+    /// define chain ID
+    inline void SetChainId(std::string chainid) {mChainId=chainid;}; 
+
+    /// return residue ID
+    inline uint GetResidId() const {return mResidId;}; 
+
+    /// define residue ID
+    inline void SetResidId(uint id) {mResidId = id;}; 
+
+    /// return atom ID
+    inline uint GetAtomId() const {return mAtomId;}; 
+
+    /// define atom ID
+    inline void SetAtomId(uint atomnumber) {mAtomId=atomnumber;}; 
+
+    /// set the extra data field
+    inline void SetExtra(std::string extra){mExtra=extra;}; 
+
+    /// get the extra data field
+    inline std::string GetExtra() const {return mExtra;}; 
 
 };
 
@@ -69,11 +93,18 @@ public:
     Atom(Atomproperty ap, Coord3D co)
             : Atomproperty(ap), mCoords(co) {};
     Coord3D GetCoords() const; ///< return atom coordinates
-    inline void SetCoords(Coord3D& coords) {mCoords=coords;}; ///< define atom coordinates
-    std::string ToString() const; ///< convert atom properties and coordinates to std::string
-    std::string ToPdbString() const ; ///< converts atom to classical PDB-like string
 
-    void Translate(const Coord3D& tr); ///< Translation of an atom
+    /// define atom coordinates
+    inline void SetCoords(Coord3D& coords) {mCoords=coords;}; 
+
+    /// convert atom properties and coordinates to std::string
+    std::string ToString() const; 
+
+    /// converts atom to classical PDB-like string
+    std::string ToPdbString() const ; 
+
+    /// Translation of an atom
+    void Translate(const Coord3D& tr); 
 
 };
 
