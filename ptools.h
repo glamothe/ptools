b4b577c46867c6536a4b24c64e2d694e623ffa0c
saladin <>
2007-08-14 13:27:00 +0000

    added the screw class to default headers

--- ptools.h
+++ ptools.h
@@ -5,6 +5,7 @@
 #ifndef PTOOLS_H
 #define PTOOLS_H
 
+#include "basetypes.h"
 #include "coord3d.h"
 #include "atom.h"
 #include "rigidbody.h"
@@ -16,6 +17,6 @@
 #include "minimizers/lbfgs_interface.h"
 #include "rmsd.h"
 #include "atomselection.h"
-
+#include "screw.h"
 
 #endif // PTOOLS_H
