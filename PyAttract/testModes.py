03758509154591fed70e9abba4d93b6d21461fc2
saladin <>
2007-08-16 20:34:43 +0000

    first file that test normal modes in action.

---
+++ PyAttract/testModes.py
@@ -0,0 +1,83 @@
+from ptools import *
+import copy
+from collections import *
+
+from Numeric import *
+
+def readModesVectors(filename='eignew.out'):
+    
+    start=0
+    
+    #reads the file:
+    modefile=open(filename, 'r')
+    filelines=modefile.readlines()
+    splittedlines = [i.split() for i in filelines ]
+    print splittedlines[0:2]
+    
+    
+    i=0
+    
+    modes=[]
+    eigens=[]
+    
+    try:
+        while(True):
+            mode=[]
+            
+            while(len(splittedlines[i])!=2):
+                mode.extend(  [ float(a) for a in  splittedlines[i]])
+                i+=1
+            #print len(mode)
+            if len(mode)!=0:
+                modes.append(copy.deepcopy(mode))
+            else:
+                eigens.append(splittedlines[i][1])
+            i=i+1
+    except:
+            pass
+    return modes, eigens
+
+
+
+modes,eigens=readModesVectors()
+print [len(i) for i in modes]
+print eigens
+print len(eigens), len(modes)
+
+
+
+#loads rigidbody:
+rig=Rigidbody('1FIN_c_r.red2')
+assert( rig.Size()*3 == len(modes[0]))
+rig=AttractRigidbody(rig)
+
+
+
+arrayOfModes = []
+
+for mode in modes:
+    vcmode = VCoord3D()
+    dmode = deque(mode)
+    try:
+        while(True):
+            co=Coord3D()
+            co.x = dmode.popleft()
+            co.y = dmode.popleft()
+            co.z = dmode.popleft()
+            vcmode.append(co)
+    except IndexError:
+            #print type(vcmode)
+            #print len(vcmode), rig.Size()
+            arrayOfModes.append(vcmode)
+
+
+for vcmode in arrayOfModes:
+    rig.addMode(vcmode)
+
+
+for l in arange(-1,1,0.1):
+    rig.applyMode(0, l)
+    WritePDB(rig, "fich%s.red"%(l))
+
+
+
