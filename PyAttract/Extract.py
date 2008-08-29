79f35caf48434d26c29db6e3f82e56a71ffb5c55
asaladin <>
2008-08-29 16:23:46 +0000

    extract predicted structures for Attract output files

---
+++ PyAttract/Extract.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+from ptools import *
+import math
+import sys
+import os
+from stat import *
+
+import shelve
+
+
+
+class StructureI:
+    def __cmp__(self, other):
+            if self.trans < other.trans:
+                return -1
+            if self.trans > other.trans:
+                return 1
+            return cmp(self.rot, other.rot)
+    pass
+
+
+
+class Extractor:
+    def __init__(self,filename):
+        #assert(isinstance(ligand, Rigidbody))
+        #self.ligand = Rigidbody(ligand)
+        
+        #check if output file has a database associated...
+        databasefile = "%s.db"%filename
+        self.d = openDatabase(filename)
+        
+        
+    def getMatrix(self, key):
+        return self.d[key].matrix
+    def getStructure(self, lig, key):
+        return rigidXMat44(lig ,self.d[key].matrix)
+
+
+
+
+def rigidXMat44(rigid, mat):
+    assert(isinstance(rigid,Rigidbody))
+    out=Rigidbody(rigid)
+    for i in range(rigid.Size()):
+        coords=rigid.GetCoords(i)
+        coords2=Coord3D()
+        coords2.x = mat[0][0]*coords.x + mat[0][1]*coords.y + mat[0][2]*coords.z + mat[0][3]
+        coords2.y = mat[1][0]*coords.x + mat[1][1]*coords.y + mat[1][2]*coords.z + mat[1][3]
+        coords2.z = mat[2][0]*coords.x + mat[2][1]*coords.y + mat[2][2]*coords.z + mat[2][3]
+        out.SetCoords(i, coords2)
+    return out
+
+
+
+
+def readStructures(file):
+
+
+    out_attach = open(file,'r')
+    #determine output format version:
+    firstline = out_attach.readline()
+    firstline = firstline.split()
+    if (len(firstline) == 0):
+        outversion = 1
+    else:
+        outversion = int(firstline[0])
+
+    #now read all lines until it finds a line like: "==        1      1   -14.0032942 94.6604593764"
+    # (version 1)
+
+    if outversion == 1 :
+        structures = {}
+        lststructures = []
+        #structures = []
+        #dicstruct = {}
+        begin=False
+        lines = out_attach.readlines()
+        for l in lines:
+            lspl = l.split()
+            if len(lspl) >0 and lspl[0] == "==":
+                begin = True
+                struct = StructureI()
+                struct.trans = int(lspl[1])
+                struct.rot = int(lspl[2])
+                struct.ener = float(lspl[3])
+                struct.rmsd = lspl[4]
+                matrix=[]
+            if (begin):
+                lspl = l.split()
+                if lspl[0]=="MAT":
+                    matrix.append( [float(lspl[i]) for i in range(1,5) ]  )
+                else:
+                    if lspl[2] == "END":
+                        begin = False   
+                        struct.matrix = matrix
+                        structures.setdefault(struct.trans,{})[struct.rot]=struct
+                        lststructures.append(struct)
+                        #structures.append(struct)
+
+
+    return structures,lststructures
+
+
+
+
+def store(structures,filename):
+    import shelve
+    sys.stderr.write("storing database\n")
+    d = shelve.open(filename)
+    for struct in structures:
+        key = "%s:%s"%(struct.trans,struct.rot)
+        d[key] = struct
+    return d
+
+
+
+def applyFromDatabase(dbfile,lig,key):
+    struct = dbfile[key]
+    
+    lig2 = rigidXMat44(lig,struct.matrix)
+    #assert( abs(float(struct.rmsd) - Rmsd(lig2.CA(),lig.CA())) < 1e-4 )
+
+    #print the pdb structure
+    pdb=""
+    for i in range(lig2.Size()):
+        atom=lig2.GetAtom(i)
+        pdb=pdb+atom.ToPdbString()
+    print pdb
+
+
+
+def openDatabase(filename):
+    databasefile = "%s.db"%filename
+    flag1 = os.path.exists(databasefile)
+    if flag1:
+        print "file exists"
+        #check to see if database is more recent than outfile
+        statdb=os.stat(databasefile)
+        statout=os.stat(filename)
+        if statdb[ST_MTIME] > statout[ST_MTIME]:  #ok database is more recent, no need to regenerate it
+            #we can use the database here :-)
+            d = shelve.open(databasefile)
+            return d
+
+        else:
+            #we must delete the database if it exists and generate a new one
+            sys.stderr.write("Removing the old database file\n")
+            if flag1: os.remove(databasefile)
+    #at this point there is no database or it has been removed
+    #we must generate it
+    sys.stderr.write("reading outfile and creating database\n")
+    structures,lststruct = readStructures(filename)
+
+    d=store(lststruct, databasefile)
+    sys.stderr.write(" done !\n")
+    return d
+ 
+
+
+def main():
+    nargs = len(sys.argv)
+    if nargs < 5:
+        print "usage: Extract 'outputfile' LigOriginalCoordinates translationNumber RotationNumber"
+        raise SystemExit
+
+    outfile = sys.argv[1]
+    ligname = sys.argv[2]
+    transNB = int(sys.argv[3])
+    rotNB =   int(sys.argv[4])
+    
+    lig = Rigidbody(ligname)
+
+    d = openDatabase(outfile)
+    applyFromDatabase(d,lig,"%s:%s"%(transNB,rotNB))
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
