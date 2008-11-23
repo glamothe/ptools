1f4960bf29843e1612a1b1473b860c68a160b458
asaladin <>
2008-11-23 11:14:56 +0000

    all python unit tests ported to c++ tests (needed for valgrind)

--- Tests/ptoolstest.h
+++ Tests/ptoolstest.h
@@ -1,6 +1,8 @@
 #include <coord3d.h>
 #include <ptools.h>
 
+#include <cstdlib>
+
 
 #include <cxxtest/TestSuite.h>
 
@@ -115,4 +117,176 @@ Coord3D A, B;
 
 };
 
+
+class TestBasicMoves: public CxxTest::TestSuite
+{
+public:
+
+Rigidbody rigid1, rigid2, rigid3;
+
+    void setUp()
+    {
+        rigid1=Rigidbody("1FIN_r.pdb");
+        rigid2=Rigidbody(rigid1);
+        rigid3=Rigidbody(rigid2);
+    }
+
+    void testBasicRmsd()
+    {
+        Rigidbody rigtmp(rigid1);
+        TS_ASSERT_EQUALS(Rmsd(rigid1, rigid1), 0.0);
+        rigid1.Translate(Coord3D(4,0,0));
+        TS_ASSERT_EQUALS(Rmsd(rigtmp, rigid1), 4);
+    }
+
+    void testTranslation1()
+    {
+        Coord3D CoM1 = rigid1.FindCenter() ;
+        rigid1.Translate(Coord3D(3.0, -55.67, 1));
+        Coord3D CoM2 = rigid1.FindCenter();
+        Coord3D diff=CoM2-CoM1;
+        TS_ASSERT( Norm2(diff + Coord3D(-3.0, 55.67, -1.0)) < 1e-6);
+        rigid1.Translate(Coord3D(-3.0, 55.67, -1.0));   //# translate back
+        TS_ASSERT(Rmsd(rigid1, rigid2) < 1e-6);
+    }
+
+    void testTranslation2(){
+        Coord3D vec1 = Coord3D (-123.54, 45.62, -99.003);
+        Coord3D vec2 = Coord3D (36.3125, 2.78, -36.378);
+        rigid2.Translate(vec1+vec2);
+        rigid2.Translate(vec1-vec2);
+        rigid2.Translate(Coord3D() - 2*vec1) ; //  #should be a global null translation + round error
+        TS_ASSERT(Rmsd(rigid2, rigid3) < 1e-6);
+    }
+
+
+};
+
+
+
+class TestCoordsArray: public CxxTest::TestSuite
+{
+
+public:
+    CoordsArray c;
+    Coord3D coo1, coo2, tr;
+
+    void setUp()
+    {
+        c = CoordsArray();
+        coo1 = Coord3D(3.0, 4.0, 5.0);
+        coo2 = Coord3D(1.0, 2.0, 7.5);
+        c.AddCoord(coo1);
+        c.AddCoord(coo2);
+        c = c;
+        tr = Coord3D(3.0, 4.5, -3.0);
+    }
+    void  testSize()
+    {
+        TS_ASSERT(c.Size() == 2);
+    }
+     
+    void  testGetAtom()
+    {
+        Coord3D c1 ;
+        c.GetCoords(0, c1);
+        TS_ASSERT(  Norm2(c1 - Coord3D(3.0, 4.0, 5.0))<1e-6 );
+    }
+
+    void  testBasicTranslation()
+    {
+        c.Translate(tr);
+        Coord3D c1;
+        Coord3D c2;
+        c.GetCoords(0, c1 );
+        c.GetCoords(1, c2 );
+        TS_ASSERT(c1 == Coord3D(6.0, 8.5, 2.0));
+    }
+
+    void  testSetCoords()
+    {
+        /*"""brief explanation:
+        For lazy evaluation, corrdinates are stored unrotated/untranslated along
+        with the rotation/translation 4x4 matrix. When user set the coordinates,
+        this means: 'change the current coordinates of atom i' and not 'change
+        the initial coordinates of atom i' so here we check that this is the case"""*/
+        c.Translate(tr); //#do some translation
+        c.AttractEulerRotate(2.0,4.0,5.0); // # do a rotation
+        Coord3D co = Coord3D(3,2,1); // #new coordinates to be added
+        c.SetCoords(0,co);
+        Coord3D co2=Coord3D();
+        c.GetCoords(0,co2); // #get the coordinates back
+        TS_ASSERT(Norm2(co-co2)<1.0e-6);
+    }
+
+};
+
+
+
+class Random
+{
+
+
+public:
+void seed(int i)
+{
+ srand(i);
+}
+
+double random()
+{
+double f =  ((double) rand()/(double)RAND_MAX);
+return f;
+}
+
+
+};
+
+
+
+
+class TestSuperposition: public CxxTest::TestSuite
+{
+public:
+
+Random random;
+Rigidbody prot1;
+
+
+
+    void setUp()
+    {
+        prot1 = Rigidbody("1FIN_r.pdb");
+        random.seed(123);
+    }
+
+    void testTransRot()
+    {
+        double x,y,z,a,b,c;
+
+        Rigidbody prot2(prot1);
+
+
+        for (int i=0; i<20; i++)
+        {
+//              #random translation coordinates:
+            x = (random.random()-0.5)*50.0;
+            y = (random.random()-0.5)*50.0;
+            z = (random.random()-0.5)*50.0;
+            prot2.Translate(Coord3D(x,y,z));
+            a = (random.random()-0.5)*50.0;
+            b = (random.random()-0.5)*50.0;
+            c = (random.random()-0.5)*50.0;
+            prot2.AttractEulerRotate(a,b,c);
+
+            Superpose_t sup = superpose(prot1,prot2); //# superpose(reference, mobile)
+            Matrix matrix = sup.matrix;
+            prot2.ApplyMatrix(matrix);
+            TS_ASSERT(Rmsd(prot2,prot1)<1e-6);
+
+        }
+
+   }
+
+};
 
