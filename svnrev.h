fabcfbc19b58e811e9c2cc5b5164022bbbea6137
sfiorucci <>
2008-11-26 11:21:50 +0000

    add libgfortran library path for 64 bits architecture

--- svnrev.h
+++ svnrev.h
@@ -2,7 +2,7 @@
  * (http://www.compuphase.com/svnrev.htm).
  * You should not modify it manually, as it may be re-generated.
  *
- * $Revision: 614M$
+ * $Revision: 614$
  * $Date: 2008-11-25$
  */
 
@@ -10,9 +10,9 @@
 #define SVNREV_H
 
 #define SVN_REV		614
-#define SVN_REVSTR	"614M"
+#define SVN_REVSTR	"614"
 #define SVN_REVDATE	"2008-11-25"
 #define SVN_REVSTAMP	20081125L
-#define SVN_MODIFIED	1
+#define SVN_MODIFIED	0
 
 #endif /* SVNREV_H */
