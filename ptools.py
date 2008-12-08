b758c8293afab42bc5ad8b5a68db5a9330c71417
asaladin <>
2008-12-08 15:31:12 +0000

    Version class more pythonic (print Version() shows nice text)

--- ptools.py
+++ ptools.py
@@ -5,12 +5,17 @@ def Coord3D_toStr(self):
 
 Coord3D.__str__ = Coord3D_toStr
 
-
 try:
     s=surreal(5)
 except:
     def surreal(i):
         return i
+
+
+def _version_print(self):
+    return "Ptools library version %i"%self.revnb
+
+Version.__str__ = _version_print
 
 
 def Rigidbody_remoteLoad(pdbname):
