a816f88041b996c4430226d5b903832527d1789e
ppoulain <>
2008-12-17 16:50:22 +0000

    Add rescale of the coarse grain van der Waals radii, tuto and script

--- svnrev.h
+++ svnrev.h
@@ -2,17 +2,17 @@
  * (http://www.compuphase.com/svnrev.htm).
  * You should not modify it manually, as it may be re-generated.
  *
- * $Revision: 614$
- * $Date: 2008-11-25$
+ * $Revision: 622$
+ * $Date: 2008-12-08$
  */
 
 #ifndef SVNREH_H
 #define SVNREV_H
 
-#define SVN_REV		614
-#define SVN_REVSTR	"614"
-#define SVN_REVDATE	"2008-11-25"
-#define SVN_REVSTAMP	20081125L
+#define SVN_REV		622
+#define SVN_REVSTR	"622"
+#define SVN_REVDATE	"2008-12-08"
+#define SVN_REVSTAMP	20081208L
 #define SVN_MODIFIED	0
 
 #endif /* SVNREV_H */
