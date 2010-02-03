9f62acb4856f4f37ab3ea7d1866711e02fcbf55e
Adrien Saladin <adrien@saladin.fr>
2010-02-03 17:17:18 +0100

    rosenbrock test function for minimizer

---
+++ Tests/rosenbrock.cpp
@@ -0,0 +1,46 @@
+//test du nouveau minimiseur
+// fonction a minimiser: (x-4)^2  au moins il existe un minimum et il existe...
+
+
+#include "forcefield.h"
+#include "lbfgs_interface.h"
+
+
+using namespace PTools;
+
+class MyFF: public ForceField
+{
+  public: 
+    dbl Function(const Vdouble& x)
+     {
+        return (1-x[0])*(1-x[0]) + 100* (x[1] - x[0]*x[0])*(x[1] - x[0]*x[0]);
+     }
+
+    void Derivatives(const Vdouble& X, Vdouble& delta)
+    {
+      
+        double t = 100 * 2 * (X[1] - X[0]*X[0]);
+	
+        delta[0] = -2*( 1 - X[0]) + t * -2 * X[0];
+	delta[1] = t ;
+    }
+
+    unsigned int ProblemSize() { return 2;}
+     
+    void initMinimization() {}
+    
+        
+     
+};
+
+int main()
+{
+  MyFF ff; 
+  Lbfgs lb(ff);
+   
+  lb.minimize(50);
+  
+  
+  
+ 
+}
