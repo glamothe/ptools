e38045f101c8ed9b01290623dfb18a6118670b50
benjamin boyer <ben.b.boyer@googlemail.com>
2010-05-17 18:54:05 +0200

    Stacking file

---
+++ Stacking.cpp
@@ -0,0 +1,25 @@
+
+#include <geometry.h>
+#include <rigidbody.h>
+#include <iostream>
+#include <Movement.h>
+#include <BasePair.h>
+#include <Stacking.h>
+#include <atomselection.h>
+using namespace std;
+
+
+namespace PTools
+{
+
+Stacking::Stacking()
+{
+
+}
+
+Stacking::~Stacking()
+{
+
+}
+
+}
---
+++ Stacking.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   Stacking.h
+ * Author: boyer
+ *
+ * Created on May 15, 2010, 5:12 AM
+ */
+
+#ifndef _STACKING_H
+#define	_STACKING_H
+
+#include "rigidbody.h"
+
+namespace PTools {
+
+
+class Stacking{
+
+private:
+    //private data
+
+
+    //private methods
+
+
+
+public:
+    Stacking();
+    ~Stacking();
+};
+
+}
+
+#endif	/* _STACKING_H */
+
