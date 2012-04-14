7ba239cb096bb392bc49e4570346b9df6f4c5d49
Adrien Saladin <adrien.saladin@gmail.com>
2012-04-14 09:36:52 +0200

    use C++ implementation of Coord3D add operator

--- ptools.pyx
+++ ptools.pyx
@@ -7,6 +7,9 @@ cdef extern from "coord3d.h" namespace "PTools":
         CppCoord3D(double nx, double ny, double nz)
         bint operator==(CppCoord3D&)
 
+    cdef CppCoord3D operator+ (CppCoord3D& A, CppCoord3D& B)
+        
+
 cdef class Coord3D:
     cdef CppCoord3D *thisptr
     def __cinit__(self, x=0, y=0, z=0):
@@ -23,8 +26,6 @@ cdef class Coord3D:
     def __richcmp__(self, other, b):
         return self._compare(other)
 
-    
-    
         
     property x:
        def __get__(self): return self.thisptr.x
@@ -36,11 +37,12 @@ cdef class Coord3D:
        def __get__(self): return self.thisptr.z
        def __set__(self,z): self.thisptr.z = z
     
-    def __add__(self, other):
-        cdef Coord3D c
-        c = Coord3D()
-        c.x = self.x + other.x
-        c.y = self.y + other.y
-        c.z = self.z + other.z
-        return c
-    
+    def __add__(Coord3D self, Coord3D other):
+        
+          cdef CppCoord3D cppself = deref(self.thisptr)
+          cdef CppCoord3D cppother = deref(other.thisptr)
+          cdef CppCoord3D cppresult = cppself+cppother
+        
+          cdef Coord3D result = Coord3D(cppresult.x, cppresult.y, cppresult.z)
+          return result
+          
