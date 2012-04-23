b5f0947d49b68245273333c7055b16400b16fe30
Adrien Saladin <adrien.saladin@gmail.com>
2012-04-23 17:33:32 +0200

    renamed ptools.so -> _ptools.so

--- ptools.py
+++ ptools.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
 from _ptools import *
 
-def Coord3D_toStr(self):
-    return PrintCoord(self)
-
-Coord3D.__str__ = Coord3D_toStr
-
-try:
-    s=surreal(5)
-except:
-    def surreal(i):
-        return i
-
-
-def _version_print(self):
-    return "Ptools library version %i"%self.revnb
-
-Version.__str__ = _version_print
-
 
 def getPDB(pdbname):
     import urllib2
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(ext_modules=[Extension(
-                   "ptools",                 # name of extension
-                   ["ptools.pyx", "cython_wrappers.cpp", "atom.cpp" , "attractrigidbody.cpp", "coordsarray.cpp", "mcopff.cpp", "rigidbody.cpp", "surface.cpp",
+                   "_ptools",                 # name of extension
+                   ["_ptools.pyx", "cython_wrappers.cpp", "atom.cpp" , "attractrigidbody.cpp", "coordsarray.cpp", "mcopff.cpp", "rigidbody.cpp", "surface.cpp",
                    "atomselection.cpp", "basetypes.cpp", "forcefield.cpp", "pairlist.cpp", "rmsd.cpp", "version.cpp",
                    "attractforcefield.cpp", "coord3d.cpp", "geometry.cpp", "pdbio.cpp", "superpose.cpp" ], #  our Cython source
                    language="c++")],  # causes Cython to create C++ source
