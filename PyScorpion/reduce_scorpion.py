12aa9fb3932b53eb8d12d828f2acfb3e0f757727
Adrien Saladin <adrien.saladin@gmail.com>
2012-06-07 17:02:59 +0200

    updated reduce_scorpion

--- PyScorpion/reduce_scorpion.py
+++ PyScorpion/reduce_scorpion.py
@@ -14,6 +14,14 @@ residNames = {}
 #keys are like this: 'ALA:CA', 
 grainMap = {}
 
+initalBeadCharge= {
+      'ASP:CB': -1,
+      'GLU:CG': -1,
+      'ARG:CG': +1, 
+      'LYS:CG': 1
+}
+
+
 
 for p in parameters:
   lspl = p.split() 
@@ -23,14 +31,21 @@ for p in parameters:
         listofgrains[lspl[5]] = 1    #add coarse grain name    
         residNames[lspl[0]] = listofgrains
 
-        grainMap["%s:%s"%(lspl[0], lspl[5])] = dict(atomname=lspl[1],  
-                                                    charge=float(lspl[2]),
-                                                    atomradius=float(lspl[3]),
-                                                    weight=float(lspl[4]),
-                                                    cgname=lspl[5],
-                                                    atomid=lspl[6],
-                                                    beadradius=float(lspl[7]),
-            )   
+        
+        listOfAtomsInAGrain = grainMap.get("%s:%s"%(lspl[0], lspl[5]), [] ) 
+
+        listOfAtomsInAGrain.append(dict(  atomname=lspl[1],  
+                                          atomcharge=float(lspl[2]),
+                                          atomradius=float(lspl[3]),
+                                          weight=float(lspl[4]),
+                                          beadname=lspl[5],
+                                          beadid=lspl[6],
+                                          beadradius=float(lspl[7]),
+                                        ) 
+                                   )
+        grainMap["%s:%s"%(lspl[0], lspl[5]) ] = listOfAtomsInAGrain
+
+ 
   except IndexError:
     pass
 
@@ -38,3 +53,22 @@ for p in parameters:
 for i,j in grainMap.items():
     print i, j
 
+
+beadCorresp = {}
+
+for residname, cgnames in residNames.items():
+    for cgname in cgnames:
+        key = "%s:%s"%(residname, cgname)
+        lstOfAllAtoms = [i['atomname'] for i in grainMap[key] if i['weight']!=0 ]
+        beadid = grainMap[key][0]['beadid']
+        beadcharge = initalBeadCharge.get(key, 0.0)
+        beadname = grainMap[key][0]['beadname']
+
+        beadDescription = [beadname, lstOfAllAtoms, beadid, beadcharge]
+    
+
+    descriptions =  beadCorresp.get(residname, [])
+    descriptions.append(beadDescription)
+    beadCorresp[residname] = descriptions
+
+print beadCorresp
