9beba94952cc7582a45132367cc05d13f4a9076e
Adrien Saladin <adrien.saladin@gmail.com>
2013-02-27 23:55:13 +0100

    removed unused files

--- Tests/heligeom/CGfromPDBFile.py
+++
@@ -1,5 +0,0 @@
-from ptools import *
-
-dna = DNA ("bp.ato.pdb","generate_B_DNA.expected") #gros grain
-
-print dna.PrintPDB()
--- Tests/heligeom/generate_B_DNA.py
+++
@@ -1,6 +0,0 @@
-from ptools import *
-
-dna = DNA ("bp.ato.pdb","AAAAAATCGATCTATC",ADNA()) #tout atom
-#dna = DNA ("bp.red.pdb","AATCGAATCG",BDNA) #gros grain
-
-print dna.PrintPDB()
