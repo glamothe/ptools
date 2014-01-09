70fc3c9d3e4c42cbeb40f2e3ea87a877b43839df
Adrien Saladin <adrien.saladin@gmail.com>
2014-01-09 16:28:48 +0100

    added option to append the chain id to contacted residues (for multi-chain pdbs)

--- PyAttract/contacted.py
+++ PyAttract/contacted.py
@@ -8,11 +8,14 @@ import sys
 import argparse
 
 
-def contacted(receptor, ligand, cutoff):
+def contacted(receptor, ligand, cutoff, withchain=False):
     resnbrec = []  #resnbrec[i] is the residue id of atom i
     for j in xrange(len(receptor)):
         at = receptor.CopyAtom(j)
-        resnbrec.append(at.residId)
+        if withchain:
+            resnbrec.append(str(at.residId) + "_" + at.chainId)
+        else:
+            resnbrec.append(at.residId)
     
     
     pl = AttractPairList(receptor,ligand, cutoff)
@@ -43,7 +46,7 @@ def main(args):
         
     else:
         #we don't have a reference for the ligand. We can only display the list of contacted residues on the receptor with this pose
-        for r in contacted(receptor, ligand, args.cutoff):
+        for r in contacted(receptor, ligand, args.cutoff, args.chain):
             print r,
             
         print ""
@@ -57,6 +60,7 @@ if __name__=="__main__":
     parser.add_argument("-l", "--ligand", dest="ligand")
     parser.add_argument("--cutoff", dest="cutoff", type=float, default=7, help="cutoff used to determine if two residues interact. Default to 7A (for coarse grained templates). You should set the cutoff to 5 when working on all atoms molecules")
     parser.add_argument('--reflig', dest="model", default=None, help="ligand model (ie correct bound solution) to calculate the percentage of contacted residues on the receptor")
+    parser.add_argument('--with-chain-id', dest="chain", default=False, action="store_true", help="add chain id to the list of contacted residues")
     
     
     args = parser.parse_args()
