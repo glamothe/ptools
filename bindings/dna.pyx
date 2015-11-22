861d36f1f01d23114dc72831c5034d092741a669
Adrien Saladin <adrien.saladin@gmail.com>
2015-11-22 11:11:00 +0100

    expose DNA class to Python

--- bindings/dna.pyx
+++ bindings/dna.pyx
@@ -1,5 +1,15 @@
 
 cdef extern from "DNA.h" namespace "PTools":
     cdef cppclass CppDNA "PTools::DNA":
-        DNA()
+        CppDNA()
 
+cdef class DNA:
+    cdef CppDNA* thisptr
+
+    def __cinit__(self):
+        self.thisptr = new CppDNA()
+
+    def __dealloc__(self):
+        if self.thisptr:
+            del self.thisptr
+            self.thisptr = <CppDNA*> 0
